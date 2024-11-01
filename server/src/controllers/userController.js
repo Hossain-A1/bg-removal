f71fa9f6bc6f1c8ed1c8b4178c081398728eca97
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-11-01 20:35:05 +0300

    error fix

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,4 +1,4 @@
-import {Webhook} from 'svix'
+import { Webhook } from "svix";
 import userModel from "../models/userModel.js";
 import { clerk_webhook_secret } from "../secret.js";
 import { successResponse } from "./responseController.js";
@@ -18,42 +18,39 @@ const clerkWebhooks = async (req, res, next) => {
       case "user.created": {
         const userData = {
           clerkId: data.id,
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
         };
 
         await userModel.create(userData);
-
+        res.json({});
         break;
       }
       case "user.updated": {
         const userData = {
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
         };
         await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+        res.json({});
         break;
       }
       case "user.deleted": {
         await userModel.findOneAndDelete({ clerkId: data.id });
+        res.json({});
         break;
       }
 
       default:
         break;
     }
-    return successResponse(res, {
-      statusCode: 200,
-      message: "user oparation successfull",
-    });
   } catch (error) {
     next(error);
   }
 };
 
-
-export {clerkWebhooks}
+export { clerkWebhooks };
