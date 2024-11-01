1195a9bc9feb7cca74f93976e2106bdb01cd6032
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-11-01 15:26:52 +0300

    done

--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,7 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import { PORT } from "./secret.js";
 
-if(process.env.NODE_ENV !== "production"){
-  app.listen(PORT, async () => {
-    console.log(`App running on port ${PORT}`);
-    await connectDB()
-  });
-}
-
+app.listen(PORT, async () => {
+  console.log(`App running on port ${PORT}`);
+  await connectDB();
+});
